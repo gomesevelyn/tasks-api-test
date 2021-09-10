27489ff64e60a4fd3b22ab37a733a10f75924a15
Evelyn Gomes <evelyngomes84@gmail.com>
2021-09-10 08:15:10 -0300

    Criacao testes de API

---
+++ src/test/java/br/ce/waquino/tasks/apitest/APITest.java
@@ -0,0 +1,58 @@
+package br.ce.waquino.tasks.apitest;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class APITest {
+	
+	//é executaado antes mesmo que a classe seja instanciada
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI = "http://localhost:8001/task-backend";
+	}
+	
+	@Test
+	public void deveRetornarTarefas() {
+		RestAssured.given()
+		.when()
+			.get("/todo")
+		.then()
+			.statusCode(200)
+		;
+	}
+	
+	@Test
+	public void deveAdicionarTarefaComSucesso() {
+		RestAssured.given()
+			.body("{\"task\": \"Teste Insercao via API\",\"dueDate\": \"2020-12-20\"}")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.statusCode(201)
+		;
+	}
+	
+	@Test
+	public void naoDeveAdicionarTarefaInvalida() {
+		RestAssured.given()
+			.body("{\"task\": \"Teste Insercao via API\",\"dueDate\": \"2010-12-20\"}")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.statusCode(400)
+			.body("message", CoreMatchers.is("Due date must not be in past")) //checa a msg de erro no responsebody
+		;
+	}
+
+}
+
+
+
+
